d047af2ea58112c2a0f78b57cda024aecc7f7eee
patrick <plyons72@gmail.com>
2018-11-04 22:25:02 -0500

    Commented code, added right and left turn functionality, and added in comment to help indicate GPIO outputs required to allow clockwise or counterclockwise motion

--- CartMotion.py
+++ CartMotion.py
@@ -6,27 +6,30 @@ import time
 # Set the mode to GPIO.Board
 GPIO.setmode(GPIO.BOARD)
 
-# Output GPIO pins for the right side of the card
-GPIO.setup(32, GPIO.OUT)
-GPIO.setup(36, GPIO.OUT)
-
 # Output GPIO pins for the left side of the cart
-GPIO.setup(38, GPIO.OUT)
-GPIO.setup(40, GPIO.OUT)
+GPIO.setup(32, GPIO.OUT) # IN_1
+GPIO.setup(36, GPIO.OUT) # IN_2
+
+# Output GPIO pins for the right side of the cart
+GPIO.setup(38, GPIO.OUT) # IN_1
+GPIO.setup(40, GPIO.OUT) # IN_2
+
+# Motor Driver Controls
+# Counterclockwise - IN_1/IN_2 = LOW/HIGH
+# Clockwise - IN_1/IN_2 = HIGH/LOW
+# Stop - IN_1/IN_2 = LOW/LOW
 
 
-# Reverse Motion #
-# Set 38 and 40 to High/Low respectively for clockwise motion
-# Set 32 and 36 to Low/High respectively for counterclockwise motion
+# Reverse Motion
 def reverse_motion():
 	low_channels = [32, 40]
 	high_channels = [36, 38]
 	GPIO.output(low_channels, GPIO.LOW)
 	GPIO.output(high_channels, GPIO.HIGH)
-	time.sleep(5)
+	time.sleep(2)
 
 
-#   Forward Motion   #
+#   Forward Motion
 # Set 32 and 36 to High/Low respectively for clockwise motion
 # Set 38 and 40 to Low/High respectively for counterclockwise motion
 def forward_motion():
@@ -34,28 +37,31 @@ def forward_motion():
 	high_channels = [32, 40]
 	GPIO.output(low_channels, GPIO.LOW)
 	GPIO.output(high_channels, GPIO.HIGH)
-	time.sleep(5)
+	time.sleep(2)
 
-# TODO
-# Right wheels will need to spin counterclockwise
-# Left wheels will need to spin clockwise
+
+# All wheels must turn counterclockwise to turn the cart right
 def turn_right():
-	low_channels = []
-	high_channels = []
-	return
+	low_channels = [32, 38]
+	high_channels = [36, 40]
+	GPIO.output(low_channels, GPIO.LOW)
+	GPIO.output(high_channels, GPIO.HIGH)
+	time.sleep(2)
 
-# TODO
-# Right wheels will need to spin counterclockwise
-# Left wheels will need to spin clockwise
+
+# All wheels must turn clockwise to turn the cart left
 def turn_left():
-	low_channels = []
-	high_channels = []
-	return
+	low_channels = [36, 40]
+	high_channels = [32, 38]
+	GPIO.output(low_channels, GPIO.LOW)
+	GPIO.output(high_channels, GPIO.HIGH)
+	time.sleep(2)
 
-
+# Stop all cart motion
 def stop_motion():
 	all_channels=[32, 36, 38, 40]
 	GPIO.output(all_channels, GPIO.LOW)
+
 
 # Drive Forward, then Reverse, then over again
 def main():
@@ -63,11 +69,13 @@ def main():
 	reverse_motion()
 
 	stop_motion()
+	time.sleep(2)
 
-	forward_motion()
-	reverse_motion()
+	turn_right()
+	turn_left()
 
 	stop_motion()
+	time.sleep(2)
 
 	# Cleanup and exit
 	GPIO.cleanup()
