c481d137e29547343a7cc1747bc52e7e55f74bb1
Joshua Lamiano <jol101@pitt.edu>
2018-11-29 21:51:20 -0500

    CartMoveController: Sets distinct duty cycles for forward/back vs. turns.
    During integration testing we found that we needed different duty cycles to
    have reliable performance for turning vs straight movements. Based on our
    experiments, we selected these values.

--- CartMoveController.py
+++ CartMoveController.py
@@ -12,8 +12,8 @@ class CartMoveController(object):
         GPIO.setup(36, GPIO.OUT) # A_IN_2
 
         # Output GPIO pins for the right side of the cart
-        GPIO.setup(38, GPIO.OUT) # B_IN_1
-        GPIO.setup(40, GPIO.OUT) # B_IN_2
+        GPIO.setup(40, GPIO.OUT) # B_IN_1
+        GPIO.setup(38, GPIO.OUT) # B_IN_2
 
         # Output GPIO pins to control the speed of the wheels
         GPIO.setup(29, GPIO.OUT) # PWM
@@ -34,57 +34,69 @@ class CartMoveController(object):
     # Reverse Motion for t seconds
     def reverse_motion(self, move_duration_secs):
 
-        # Right Wheel Counter-Clockwise
+        self.pwm.ChangeDutyCycle(20)
+        # Right Wheel Clockwise
         GPIO.output(32, GPIO.LOW)
         GPIO.output(36, GPIO.HIGH)
 
-        # Left Wheel Clockwise
-        GPIO.output(38, GPIO.HIGH)
-        GPIO.output(40, GPIO.LOW)
+        # Left Wheel CCL
+        GPIO.output(40, GPIO.HIGH)
+        GPIO.output(38, GPIO.LOW)
 
         time.sleep(move_duration_secs)
-
+        self.halt_motion()
 
     #   Forward Motion for t seconds
     #   Right spins clockwise, left counterclockwise
     def forward_motion(self, move_duration_secs):
 
+        self.pwm.ChangeDutyCycle(20)
         # Right Wheel Clockwise
         GPIO.output(32, GPIO.HIGH)
         GPIO.output(36, GPIO.LOW)
 
         # Left Wheel CCL
-        GPIO.output(38, GPIO.LOW)
-        GPIO.output(40, GPIO.HIGH)
+        GPIO.output(40, GPIO.LOW)
+        GPIO.output(38, GPIO.HIGH)
 
         time.sleep(move_duration_secs)
-
+        self.halt_motion()
 
 
     # All wheels must turn counterclockwise to turn the cart right for t seconds
     def turn_right(self, move_duration_secs):
 
+        self.pwm.ChangeDutyCycle(40)
         # Right Wheel CCL
         GPIO.output(32, GPIO.LOW)
         GPIO.output(36, GPIO.HIGH)
 
         # Left Wheel CCL
-        GPIO.output(38, GPIO.LOW)
-        GPIO.output(40, GPIO.HIGH)
+        GPIO.output(40, GPIO.LOW)
+        GPIO.output(38, GPIO.HIGH)
 
         time.sleep(move_duration_secs)
-
+        self.halt_motion()
 
 
     # All wheels must turn clockwise to turn the cart left for t seconds
     def turn_left(self, move_duration_secs):
 
+        self.pwm.ChangeDutyCycle(40)
         # Right Wheel Clockwise
         GPIO.output(32, GPIO.HIGH)
         GPIO.output(36, GPIO.LOW)
 
         # Left Wheel Clockwise
-        GPIO.output(38, GPIO.HIGH)
-        GPIO.output(40, GPIO.LOW)
+        GPIO.output(40, GPIO.HIGH)
+        GPIO.output(38, GPIO.LOW)
 
         time.sleep(move_duration_secs)
+        self.halt_motion()
+
+    def halt_motion(self):
+        GPIO.output(32, GPIO.LOW)
+        GPIO.output(36, GPIO.LOW)
+        GPIO.output(40, GPIO.LOW)
+        GPIO.output(38, GPIO.LOW)
+
