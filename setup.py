49fc47f501179799eb2bc70ba7d4f2879411e336
Lindsey <marralindseyj@gmail.com>
2018-12-01 19:19:41 -0500

    Updates version number in preparation for release 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='cart-motion',
-      version='0.1',
+      version='0.2',
       description='library for moving the cart',
       py_modules=['CartMoveController']
     )
